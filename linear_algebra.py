f381e9e97d43256c18758df1616ec3ac52ebed85
sateigen <shannon.teigen@gmail.com>
2016-07-20 16:08:23 -0400

    Working functions for comparing the shape of any number of vectors and dot product.

--- linear_algebra.py
+++ linear_algebra.py
@@ -21,4 +21,28 @@ def vector_sub(vector1, vector2):
 
 
 def vector_sum(*args):
-    return [sum(values) for values in zip(*args)]
+    if not compare_shapes(*args):
+        raise ShapeError
+    else:
+        return [sum(values) for values in zip(*args)]
+
+
+def compare_shapes(*args):
+    return len(set([shape(item) for item in args])) == 1
+
+
+def dot(vector1, vector2):
+    if shape(vector1) != shape(vector2):
+        raise ShapeError
+    else:
+        return sum([value1 * value2 for value1, value2 in zip(vector1, vector2)])
+
+
+# def test_dot():
+#     """
+#     dot([a b], [c d])   = a * c + b * d
+#     dot(Vector, Vector) = Scalar
+#     """
+#     assert dot(w, y) == 160
+#     assert dot(m, n) == 15
+#     assert dot(u, z) == 0
