4c5769cfc77cbb56b384b9de72ae5c18e2d6ffd4
sateigen <shannon.teigen@gmail.com>
2016-07-20 15:39:10 -0400

    Working functions for adding two vectors, subtracting two vectors, and adding any number of vectors together.

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,2 +1,24 @@
+class ShapeError(Exception):
+    pass
+
+
 def shape(vector):
     return(len(vector), )
+
+
+def vector_add(vector1, vector2):
+    if shape(vector1) != shape(vector2):
+        raise ShapeError
+    else:
+        return [sum(values) for values in zip(vector1, vector2)]
+
+
+def vector_sub(vector1, vector2):
+    if shape(vector1) != shape(vector2):
+        raise ShapeError
+    else:
+        return [item1 - item2 for item1, item2 in zip(vector1, vector2)]
+
+
+def vector_sum(*args):
+    return [sum(values) for values in zip(*args)]
